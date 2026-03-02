9c69d7db247fa15ead24199f049adeee56947721
Harry Bahatan <20238447@s.ubaguio.edu>
2026-03-02 09:37:25 +0800

    Added Interpolation adn Property-Binding Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { AboutComponent } from './comps/about/about.component';
 import { HomeComponent } from './comps/home/home.component';
 import { UserInteractionComponent } from './comps/user-interaction/user-interaction.component';
 import { GradeCalculatorComponent } from './comps/grade-calculator/grade-calculator.component';
+import { InterpolationComponent } from './comps/interpolation/interpolation.component';
+import { PropertyBindingComponent } from './comps/property-binding/property-binding.component';
 export const routes: Routes = [
     
   { path: '', component: HomeComponent },
@@ -11,7 +13,9 @@ export const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path : 'login',component : LoginComponent},
    { path: 'user-interaction', component: UserInteractionComponent },
-    { path: 'grade-calculator', component: GradeCalculatorComponent }
+    { path: 'grade-calculator', component: GradeCalculatorComponent },
+    { path: 'interpolation', component: InterpolationComponent },
+    { path: 'property-binding', component: PropertyBindingComponent }
    
 ];
 
